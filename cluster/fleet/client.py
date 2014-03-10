2efd35ac47d02646e436307bc2ede4a4ff25f985
Ashald <xashald@gmail.com>
2014-03-10 19:22:41 +0200

    Code refactoring - Request. Renamed class members to distinguish between 'public' and 'non-public' members. Also changed some field names to include some semantic meaning.

--- cluster/fleet/client.py
+++ cluster/fleet/client.py
@@ -4,24 +4,25 @@ from member import Component
 
 class Request(Component):
 
-    client_ids = iter(xrange(1000000, sys.maxint))
+    # TODO: consider using itertools.count(10**6) instead of iter(xrange(10**6, sys.maxint))
+    client_ids = iter(xrange(10**6, sys.maxint))
     RETRANSMIT_TIME = 0.5
 
     def __init__(self, member, n, callback):
         super(Request, self).__init__(member)
-        self.client_id = self.client_ids.next()
-        self.n = n
-        self.output = None
-        self.callback = callback
+        self._client_id = next(self.client_ids)
+        self._n = n  # TODO: rename - use name with semantic meaning
+        self._invoke_timer = None
+        self._completed_callback = callback
 
     def start(self):
-        self.send([self.address], 'INVOKE', caller=self.address, client_id=self.client_id, input_value=self.n)
-        self.invoke_timer = self.set_timer(self.RETRANSMIT_TIME, self.start)
+        self.send([self.address], 'INVOKE', caller=self.address, client_id=self._client_id, input_value=self._n)
+        self._invoke_timer = self.set_timer(self.RETRANSMIT_TIME, self.start)
 
     def do_INVOKED(self, client_id, output):
-        if client_id != self.client_id:
+        if client_id != self._client_id:
             return
-        self.logger.debug("received output %r" % (output,))
-        self.cancel_timer(self.invoke_timer)
-        self.callback(output)
+        self.logger.debug("received output %r", output)
+        self.cancel_timer(self._invoke_timer)
+        self._completed_callback(output)
         self.stop()
