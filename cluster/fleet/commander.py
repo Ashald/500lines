db7d006f9f0721a9926f7f8aaa2896dcb6aa71fa
Ashald <xashald@gmail.com>
2014-03-10 19:38:20 +0200

    Code refactoring - Commander. Renamed class members to distinguish between 'public' and 'non-public' members.

--- cluster/fleet/commander.py
+++ cluster/fleet/commander.py
@@ -6,44 +6,46 @@ class Commander(Component):
 
     def __init__(self, member, leader, ballot_num, slot, proposal, commander_id, peers):
         super(Commander, self).__init__(member)
-        self.leader = leader
-        self.ballot_num = ballot_num
-        self.slot = slot
-        self.proposal = proposal
-        self.commander_id = commander_id
-        self.accepted = set([])
-        self.peers = peers
-        self.quorum = len(peers) / 2 + 1
-        self.timer = None
+        self._leader = leader
+        self._ballot_num = ballot_num
+        self._slot = slot
+        self._proposal = proposal
+        self._commander_id = commander_id
+        self._accepted = set()
+        self._peers = peers
+        self._quorum = len(peers) / 2 + 1
+        self._commander_timer = None
 
     def start(self):
-        self.send(set(self.peers) - self.accepted, 'ACCEPT',  # p2a
-                  commander_id=self.commander_id,
-                  ballot_num=self.ballot_num,
-                  slot=self.slot,
-                  proposal=self.proposal)
-        self.timer = self.set_timer(ACCEPT_RETRANSMIT, self.start)
+        self.send(set(self._peers) - self._accepted, 'ACCEPT',  # p2a
+                  commander_id=self._commander_id,
+                  ballot_num=self._ballot_num,
+                  slot=self._slot,
+                  proposal=self._proposal)
+        self._commander_timer = self.set_timer(ACCEPT_RETRANSMIT, self.start)
 
-    def finished(self, ballot_num, preempted):
-        self.leader.commander_finished(self.commander_id, ballot_num, preempted)
-        if self.timer:
-            self.cancel_timer(self.timer)
+    def _finished(self, ballot_num, preempted):
+        self._leader.commander_finished(self._commander_id, ballot_num, preempted)
+        if self._commander_timer:
+            self.cancel_timer(self._commander_timer)
         self.stop()
 
     def do_ACCEPTED(self, commander_id, acceptor, ballot_num):  # p2b
-        if commander_id != self.commander_id:
+        if commander_id != self._commander_id:
             return
-        if ballot_num == self.ballot_num:
-            self.accepted.add(acceptor)
-            if len(self.accepted) < self.quorum:
+
+        preempted = True
+        if ballot_num == self._ballot_num:
+            self._accepted.add(acceptor)
+            if len(self._accepted) < self._quorum:
                 return
             # make sure that this node hears about the decision, otherwise the
             # slot can get "stuck" if all of the DECISION messages get lost, or
             # if this node is not in self.peers
-            self.event('decision', slot=self.slot, proposal=self.proposal)
-            self.send(self.peers, 'DECISION',
-                      slot=self.slot,
-                      proposal=self.proposal)
-            self.finished(ballot_num, False)
-        else:
-            self.finished(ballot_num, True)
+            self.event('decision', slot=self._slot, proposal=self._proposal)
+            self.send(self._peers, 'DECISION',
+                      slot=self._slot,
+                      proposal=self._proposal)
+            preempted = False
+
+        self._finished(ballot_num, preempted)
