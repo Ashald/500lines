93d85249cbddbd9b92470ce8d7cdf2ccee6c5438
Ashald <xashald@gmail.com>
2014-03-13 21:54:53 +0200

    Code refactoring - Heartbeat. Renamed class members to distinguish between 'public' and 'non-public' members.

--- cluster/fleet/heartbeat.py
+++ cluster/fleet/heartbeat.py
@@ -6,31 +6,31 @@ class Heartbeat(Component):
 
     def __init__(self, member, clock):
         super(Heartbeat, self).__init__(member)
-        self.running = False
-        self.last_heard_from = {}
-        self.peers = None
-        self.clock = clock
+        self._running = False
+        self._last_heard_from = {}
+        self._peers = None
+        self._clock = clock
 
     def on_view_change_event(self, slot, view_id, peers):
-        self.peers = set(peers)
-        for peer in self.peers:
-            self.last_heard_from[peer] = self.clock()
-        if not self.running:
-            self.heartbeat()
-            self.running = True
+        self._peers = set(peers)
+        self._last_heard_from.update({peer: self._clock() for peer in self._peers})
+
+        if not self._running:
+            self._heartbeat()
+            self._running = True
 
     def do_HEARTBEAT(self, sender):
-        self.last_heard_from[sender] = self.clock()
+        self._last_heard_from[sender] = self._clock()
 
-    def heartbeat(self):
+    def _heartbeat(self):
         # send heartbeats to other nodes
-        self.send(self.peers, 'HEARTBEAT', sender=self.address)
+        self.send(self._peers, 'HEARTBEAT', sender=self.address)
 
         # determine if any peers are down, and notify if so; note that this
         # notification will occur repeatedly until a view change
-        too_old = self.clock() - HEARTBEAT_GONE_COUNT * HEARTBEAT_INTERVAL
-        active_peers = set(p for p in self.last_heard_from if self.last_heard_from[p] >= too_old)
-        if active_peers != self.peers:
-            self.event('peers_down', down=self.peers - active_peers)
+        too_old = self._clock() - HEARTBEAT_GONE_COUNT * HEARTBEAT_INTERVAL
+        active_peers = set(p for p in self._last_heard_from if self._last_heard_from[p] >= too_old)
+        if active_peers != self._peers:
+            self.event('peers_down', down=self._peers - active_peers)
 
-        self.set_timer(HEARTBEAT_INTERVAL, self.heartbeat)
+        self.set_timer(HEARTBEAT_INTERVAL, self._heartbeat)
