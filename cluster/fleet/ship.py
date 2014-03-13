2acb90a937d07580b9b4e7ff425dfeed6d741a0e
Ashald <xashald@gmail.com>
2014-03-13 23:59:04 +0200

    Code refactoring - ship.py. Renamed class members to distinguish between 'public' and 'non-public' members.

--- cluster/fleet/ship.py
+++ cluster/fleet/ship.py
@@ -10,48 +10,48 @@ class Listener(member.Component):
 
     def __init__(self, member, event_queue):
         super(Listener, self).__init__(member)
-        self.event_queue = event_queue
+        self._event_queue = event_queue
 
     def on_view_change_event(self, slot, view_id, peers):
-        self.event_queue.put(("membership_change", peers))
+        self._event_queue.put(("membership_change", peers))
 
 
 class Ship(object):
 
     def __init__(self, state_machine, port=10001, peers=None, seed=None):
         peers = peers or ['255.255.255.255-%d' % port]
-        self.node = network.Node(port)
+        self._node = network.Node(port)
         if seed is not None:
-            self.cluster_member = member_replicated.ClusterSeed(
-                self.node, seed)
+            self._cluster_member = member_replicated.ClusterSeed(self._node, seed)
         else:
-            self.cluster_member = member_replicated.ClusterMember(
-                self.node, state_machine, peers=peers)
-        self.event_queue = Queue.Queue()
-        self.current_request = None
-        self.listener = Listener(self.cluster_member, self.event_queue)
+            self._cluster_member = member_replicated.ClusterMember(self._node, state_machine, peers=peers)
+        self._event_queue = Queue.Queue()
+        self._current_request = None
+        self._listener = Listener(self._cluster_member, self._event_queue)
+
+        self._thread = None
 
     def start(self):
         def run():
-            self.cluster_member.start()
-            self.node.run()
+            self._cluster_member.start()
+            self._node.run()
 
-        self.thread = threading.Thread(target=run)
-        self.thread.setDaemon(1)
-        self.thread.start()
+        self._thread = threading.Thread(target=run)
+        self._thread.setDaemon(True)
+        self._thread.start()
 
     def invoke(self, input_value):
-        assert self.current_request is None
+        assert self._current_request is None
         q = Queue.Queue()
 
         def done(output):
-            self.current_request = None
+            self._current_request = None
             q.put(output)
-        self.current_request = client.Request(self.cluster_member, input_value, done)
-        self.current_request.start()
+
+        self._current_request = client.Request(self._cluster_member, input_value, done)
+        self._current_request.start()
         return q.get()
 
     def events(self):
         while True:
-            evt = self.event_queue.get()
-            yield evt
+            yield self._event_queue.get()
