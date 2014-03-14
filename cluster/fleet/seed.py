e4775eef1d7b7f1eb75fb7812aa89757f69f9eec
Ashald <boris.perov@webriders.com.ua>
2014-03-14 09:03:24 +0200

    Code refactoring - Seed. Renamed class members to distinguish between 'public' and 'non-public' members.

--- cluster/fleet/seed.py
+++ cluster/fleet/seed.py
@@ -1,4 +1,4 @@
-from . import defaultlist, ALPHA, JOIN_RETRANSMIT
+from . import ALPHA, JOIN_RETRANSMIT
 from .member import Component
 
 
@@ -9,34 +9,34 @@ class Seed(Component):
 
     def __init__(self, member, initial_state):
         super(Seed, self).__init__(member)
-        self.initial_state = initial_state
-        self.peers = set([])
-        self.exit_timer = None
+        self._initial_state = initial_state
+        self._peers = set()
+        self._exit_timer = None
 
     def do_JOIN(self, requester):
         # three is the minimum membership for a working cluster, so don't
         # respond until then, but don't expand the peers list beyond 3
-        if len(self.peers) < 3:
-            self.peers.add(requester)
-            if len(self.peers) < 3:
+        if len(self._peers) < 3:
+            self._peers.add(requester)
+            if len(self._peers) < 3:
                 return
 
         # otherwise, we have a cluster, but don't welcome any nodes not
         # part of that cluster (the cluster can do that itself)
-        if requester not in self.peers:
+        if requester not in self._peers:
             return
 
-        peer_history = dict((sl, self.peers) for sl in range(0, ALPHA))
-        self.send(self.peers, 'WELCOME',
-                  state=self.initial_state,
+        peer_history = {sl: self._peers for sl in xrange(0, ALPHA)}
+        self.send(self._peers, 'WELCOME',
+                  state=self._initial_state,
                   slot_num=ALPHA,
-                  decisions=defaultlist(),
+                  decisions=[],
                   view_id=0,
-                  peers=list(self.peers),
+                  peers=list(self._peers),
                   peer_history=peer_history.copy())
 
         # stick around for long enough that we don't hear any new JOINs from
         # the newly formed cluster
-        if self.exit_timer:
-            self.cancel_timer(self.exit_timer)
-        self.exit_timer = self.set_timer(JOIN_RETRANSMIT * 2, self.stop)
+        if self._exit_timer:
+            self.cancel_timer(self._exit_timer)
+        self._exit_timer = self.set_timer(JOIN_RETRANSMIT * 2, self.stop)
